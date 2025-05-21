7c00f18e1a87b0222cbe8bfd2613d6395ba1adba
Anete Kupča <anete.kupca@edu.rtu.lv>
2025-05-21 15:25:12 +0300

    pārveidoju programmu, lai tiktu izmantotas selenium un webdriver_manager bibliotēkas, jo lapa ielādējas izmantojot javascript. Pašlaik programma nolasa un izvada uzņēmumu nosaukumus, kuriem tiek piedāvāta atlaide

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-import requests
-from bs4 import BeautifulSoup 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 
-url = "https://isic.lv/lv/atlaides/"
-lapa = requests.get(url)
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-if lapa.status_code == 200:
-    lapas_saturs = BeautifulSoup(lapa.text, "html.parser")
-    print(lapas_saturs.prettify())
+driver.get("https://isic.lv/lv/atlaides/")
+
+try:
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'discount-block-item')))
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('a', class_='discount-block-item')
+
+    print("Atlaides nosaukumi:")
+    for i in items:
+        t = i.find('span', class_='title')
+        if t:
+            print("-", t.text.strip())
+except:
+    print("Neizdevās ielādēt lapu")
+
+driver.quit()
