f0e8713a81215d5c4fe0b30ad53e3766fc4d75f6
Anete Kupča <anete.kupca@edu.rtu.lv>
2025-05-21 15:49:48 +0300

    pamanīju ka netika izvadītas visas atlaides, tāpēc importēju time bibliotēku un pievienoju skriptu, kas notin programmu līdz pašām beigām, tādējādi nolasot visas pieejamās atlaides

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+import time
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 driver.get("https://isic.lv/lv/atlaides/")
+
+scroll_pause = 1
+last_height = driver.execute_script("return document.body.scrollHeight")
+for i in range(10):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(scroll_pause)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
 
 try:
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'discount-block-item')))
@@ -18,7 +31,7 @@ try:
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('a', class_='discount-block-item')
 
-    print("Atlaides nosaukumi:")
+    print("Atlaides:")
     for i in items:
         t = i.find('span', class_='title')
         if t:
